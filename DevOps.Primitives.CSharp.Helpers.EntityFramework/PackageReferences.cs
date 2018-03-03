5c8699f4fec9ce2da60a330f4e93de01cebfc60a
Christopher Dorst <chris@dorst.email>
2018-03-02 23:35:00 -0600

    Update dependency versions

--- DevOps.Primitives.CSharp.Helpers.EntityFramework/PackageReferences.cs
+++ DevOps.Primitives.CSharp.Helpers.EntityFramework/PackageReferences.cs
@@ -5,7 +5,7 @@ namespace DevOps.Primitives.CSharp.Helpers.EntityFramework
     public static class PackageReferences
     {
         public static NuGetReference CDorstDevOpsPrimitivesStrings() => new NuGetReference("CDorst.DevOps.Primitives.Strings", "1.0.4");
-        public static NuGetReference MicrosoftEntityFrameworkCore() => new NuGetReference("Microsoft.EntityFrameworkCore", "2.0.1");
-        public static NuGetReference ProtobufNet() => new NuGetReference("protobuf-net", "2.3.3");
+        public static NuGetReference MicrosoftEntityFrameworkCore() => new NuGetReference("Microsoft.EntityFrameworkCore", "2.1.0-preview1-final");
+        public static NuGetReference ProtobufNet() => new NuGetReference("protobuf-net", "2.3.7");
     }
 }
