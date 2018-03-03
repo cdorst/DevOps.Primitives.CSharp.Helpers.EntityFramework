f88de0c9b3d5ea5f6886d7b304cbab6914c87603
Christopher Dorst <chris@dorst.email>
2018-03-03 00:07:22 -0600

    Update dependency; remove package reference

--- DevOps.Primitives.CSharp.Helpers.EntityFramework/PackageReferences.cs
+++ DevOps.Primitives.CSharp.Helpers.EntityFramework/PackageReferences.cs
@@ -4,7 +4,6 @@ namespace DevOps.Primitives.CSharp.Helpers.EntityFramework
 {
     public static class PackageReferences
     {
-        public static NuGetReference CDorstDevOpsPrimitivesStrings() => new NuGetReference("CDorst.DevOps.Primitives.Strings", "1.0.4");
         public static NuGetReference MicrosoftEntityFrameworkCore() => new NuGetReference("Microsoft.EntityFrameworkCore", "2.1.0-preview1-final");
         public static NuGetReference ProtobufNet() => new NuGetReference("protobuf-net", "2.3.7");
     }
